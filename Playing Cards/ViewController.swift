668f51a03eaf8d4806adfbe3e8d5742506f37910
salimzhanova <salimzhanova@kolesa.kz>
2026-02-08 18:56:54 +0500

    created 2nd project

---
+++ Playing Cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Playing Cards
+//
+//  Created by kamila on 08.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
